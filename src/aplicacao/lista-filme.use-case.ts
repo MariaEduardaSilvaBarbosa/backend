a83d86c293b66e1dd0917428444afad902ac810a
MariaEduardaSilvaBarbosa <maria.barbosa4@estudante.ifms.edu.br>
2023-11-21 11:05:58 -0400

    salva filme e lista filme

---
+++ src/aplicacao/lista-filme.use-case.ts
@@ -0,0 +1,13 @@
+import FilmeRepositorioInterface from "./filme-repositorio-interface";
+
+export default class ListaFilme{
+      //construtor
+      constructor(
+        readonly filmeRepositorio:FilmeRepositorioInterface
+        ){}
+
+      //executar
+      public async executar(){
+        return this.filmeRepositorio.listar()
+      }
+}
